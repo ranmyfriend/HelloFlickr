355a5114fb1938b76802947ae8ddeb48b8826e60
ranjit <ranjithkumar2010a@live.com>
2017-07-04 21:27:05 +0530

    Initial Commit

---
+++ HelloFlickr/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloFlickr
+//
+//  Created by Ranjith Kumar on 7/4/17.
+//  Copyright © 2017 Ranjith Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
